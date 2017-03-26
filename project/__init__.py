54f0d226c655bd77675fb9f62341f7a34f3c2a10
Tim Garcia <tim.m.garcia@gmail.com>
2017-03-25 18:53:03 -0700

    Record and upload wav format files

--- project/__init__.py
+++ project/__init__.py
@@ -1,15 +1,20 @@
 from flask import Flask, Blueprint, redirect, render_template, request, flash, url_for, jsonify, json
 from flask_wtf.csrf import CSRFProtect
+from werkzeug.utils import secure_filename
 import os
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'talkingcats'
-
+app.config['UPLOAD_FOLDER'] = os.environ.get('UPLOAD_FOLDER') or './'
 debug = True
 
 if os.environ.get('ENV') == 'production':
     debug = False
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in set(['wav'])
 
 @app.route('/')
 def root():
@@ -17,4 +22,10 @@ def root():
 
 @app.route('/sound', methods=["POST"])
 def sound():
-    return "hello"
+    data = request.files['data']
+    if data:
+        filename = secure_filename("test.wav")
+        data.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return json.dumps({"text": "We Got Data!"})
+
+    return json.dumps({"text": "Something went wrong"}), 400
---
+++ project/static/js/index.js
@@ -0,0 +1,55 @@
+var mediaConstraints = {
+    audio: true
+};
+
+let mediaRecorder;
+let $recordBtn = $("#record");
+let fd;
+const DATA = 'data';
+const FILENAME = 'filename';
+function onMediaError(e) {
+  console.error('media error', e);
+}
+
+
+function onMediaSuccess(stream) {
+  fd = new FormData();
+  mediaRecorder = new MediaStreamRecorder(stream);
+  mediaRecorder.mimeType = 'audio/wav';
+  mediaRecorder.ondataavailable = function(blob) {
+    fd.append(DATA, blob)
+  };
+  mediaRecorder.onstop = function() {
+    $recordBtn.removeClass();
+    $recordBtn.addClass("btn btn-default");
+    $recordBtn.html("RECORD");
+
+    $.ajax({
+      type: 'POST',
+      url: '/sound',
+      data: fd,
+      processData: false,
+      contentType: false
+    }).done(function(data) {
+       console.log(data);
+    });
+  };
+
+  mediaRecorder.start();
+}
+
+
+
+$recordBtn.click(function(e) {
+  if ($recordBtn.html() === 'RECORD') {
+    $recordBtn.removeClass();
+    $recordBtn.addClass("btn btn-danger");
+    $recordBtn.html("STOP REC")
+    navigator.getUserMedia(mediaConstraints, onMediaSuccess, onMediaError);
+  } else {
+    if (mediaRecorder) {
+      mediaRecorder.stop();
+      mediaRecorder.onstop();
+    }
+  }
+});
